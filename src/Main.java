901cc27ae196caf8de7cfe907296909c8fc06ea1
Xaria A <xaria.shella@gmail.com>
2022-09-01 09:51:31 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("    _____");
+        System.out.println("   (___  ' .");
+        System.out.println("    /_____)");
+        System.out.println("   ()@ @ ))))");
+        System.out.println("    'C   ,()(()");
+        System.out.println("   ,.'_'.'     ");
+        System.out.println("__/ )   (--'");
+        System.out.println("'._./     ");
+        System.out.println("  (-._._._)");
+        System.out.println("  _||   ||_");
+        System.out.println("  (__.).__)");
+    }
+}
